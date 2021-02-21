8a8e66231fedf0d02ba015dbc9578a87a2a6d2df
Mitchell Bean <mitchtroybean@gmail.com>
2021-02-21 11:33:07 -0800

    main card working

--- javascript.js
+++ javascript.js
@@ -7,7 +7,8 @@ var queryTerm = "";
 var queryURL =
   "https://api.openweathermap.org/data/2.5/weather?appid=" +
   authKey +
-  queryTerm;
+  queryTerm +
+  "&units=imperial";
 //TODO: create functions
 var data = "";
 //TODO: get first search data from api rolling 2 call to one api
@@ -22,13 +23,33 @@ function runQuery(newURL) {
     console.log(data.name);
     console.log(data.coord);
     var citytag = data.name;
-    $("#headw").append(citytag);
+    $("#headw").append(citytag + " ");
+
+    //get date
     var wrongtime = data.dt;
     var rightTime = new Date(wrongtime * 1000);
-    $("#headw").append(rightTime);
+    dateString = rightTime.toString().split(" ").slice(0, 4).join(" ");
+    console.log(dateString);
+    $("#headw").append(dateString);
     var wtag = data.weather[0].icon;
-    $("#headw").append(wtag);
 
+    //icon
+    var iconurl = "http://openweathermap.org/img/w/" + wtag + ".png";
+    $("#wicon").attr("src", iconurl);
+
+    //temp
+    var temp = data.main.temp;
+    $("#temp").append("Temperature: " + temp + String.fromCharCode(176) + "F");
+
+    //humid
+    var humid = data.main.temp;
+    $("#humid").append("Humidity: " + humid + "%");
+
+    //wind
+    var wind = data.wind.speed;
+    $("#wind").append("Wind: " + wind);
+
+    //coord for 2nd call
     coord = data.coord;
     sunAjaxtwo(coord);
   });
@@ -47,6 +68,10 @@ function runQuery(newURL) {
 
       console.log(finalData);
 
+      //uv
+      var uvIndex = finalData.current.uvi;
+      $("#uv").append("UV index: " + uvIndex);
+
       // console.log(finalData.name);
       // console.log(finalData.weather[0].description);
     });
@@ -57,6 +82,8 @@ function runQuery(newURL) {
 $("#searchBtn").on("click", function (event) {
   event.preventDefault();
   $("#headw").empty();
+  $("#wicon").empty();
+
   queryTerm = $("#searchplz").val();
 
   console.log(queryTerm);
